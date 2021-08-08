f3bde22ecbc6a55f43540156db01776e75e69b98
ArunKumarSJ <88589077+ArunKumarSJ@users.noreply.github.com>
2021-08-08 13:45:55 +0530

    Add files via upload

---
+++ PrimeNumber.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class PrimeNumber {
+
+	public static void main(String[] args) {
+		int n=13;
+		boolean prime=true;
+		for(int i=2;i<n;i++) {
+			if(n%i==0)
+				prime=false;
+			   break;
+		}
+		System.out.println(prime);
+	}
+
+}
