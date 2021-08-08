5b9fd475f7408af83e47be4e5a1b4599a20e70b8
ArunKumarSJ <88589077+ArunKumarSJ@users.noreply.github.com>
2021-08-08 12:06:41 +0530

    Add files via upload

---
+++ FactorialNum.java
@@ -0,0 +1,15 @@
+package week1.day1;
+
+public class FactorialNum 
+{
+public static void main(String[] args) 
+{
+	int n=5;
+	int fact=1,i;
+	for (i=1;i<=n;i++)
+	{
+		fact = fact*i;
+	}
+	 System.out.println(fact);
+}
+}
